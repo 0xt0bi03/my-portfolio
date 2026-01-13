89d2e132aeb3b0e6f3b74d1824fce1031c5a2176
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-13 12:16:16 +0530

    made changes

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ let skills = document.querySelector('.skills');
 let projects = document.querySelector('.projects');
 let interests = document.querySelector('.interests');
 
+let connections = new Map([[btnstate[0], about], [btnstate[1], skills], [btnstate[2], projects], [btnstate[3], interests]]);
+
+about.style.display = 'block';
+skills.style.display = 'none';
+projects.style.display = 'none';
+interests.style.display = 'none';
+
+
+
 btnstate.forEach(function(btn)
 {
     btn.addEventListener('click', function()
     {
-        console.log(`button ${btn.textContent} is clicked!`);
+        // console.log(`button ${connections.get(btn).textContent} is clicked!`);
+
+        // btnstate.forEach(function (b)
+        // {
+        //     b.classList.remove('active');
+        // });
+        // btn.classList.add('active');
+
+        connections.forEach(function (content) 
+        {
+            content.style.display = "none";
+        });
+
+        connections.get(btn).style.display = 'block';
+        // btn.style.d
+
     });
 });
