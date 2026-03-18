defa74df5a0cc504abd94db7c13bf7ab5f336f7d
tobi <m4sk3d.t0bi@gmail.com>
2026-03-18 23:37:21 +0530

    made changes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // SECTION SWITCHING (ABOUT / SKILLS / PROJECTS / INTERESTS)
 // ==================================================
 
-
 const btnstate = document.querySelectorAll('.btns');
 
 const about = document.querySelector('.about');
